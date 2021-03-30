91e9f63c49ba4e84eed2b11a049c0c463bccab30
Doomer77 <newzibkoff1984@gmail.com>
2021-03-30 23:17:45 +0300

    Start developing your own CMS

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+        'admin_email' => 'pjparts@yandex.ru',
+        'shop_name' => 'PJParts',
+        'pagination' => 3,
+        'smtp_login' => '',
+        'smtp_password' => '',
+    ];
